1286bf8cc7d09eb8a9e5e1830a5b56ee7d83d549
roxaline <mariadelarosita078@gmail.com>
2019-04-05 12:25:49 +0200

    add elements in REST_FRAMEWORK = {

--- awapp/settings.py
+++ awapp/settings.py
@@ -129,6 +129,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
